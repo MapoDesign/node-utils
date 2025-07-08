5dce80139d7a6b257b4485d6a39eb71fef5383d2
MapoDesign <elisaresmini@gmail.com>
2025-07-08 12:25:21 +0200

    update getWebsite - https module

--- components/getWebsite.js
+++ components/getWebsite.js
@@ -1,12 +1,37 @@
 const http = require("http");
+const https = require("https");
 const path = require("path");
+// const options = {
+//   hostname: "info.cern.cch",
+//   method: "GET",
+//   path: "/hypertext/WWW/TheProject.html",
+// };
+
+// const req = http
+//   .request(options, (res) => {
+//     if (res.statusCode < 300 && res.statusCode >= 200) {
+//       console.log(`Request successful: ${res.statusCode}`);
+//       res.on("data", (chunk) => {
+//         process.stdout.write(chunk);
+//       });
+//     } else {
+//       console.log(`Request failed: ${res.statusCode}`);
+//     }
+//   })
+//   .on("error", (err) => {
+//     console.error(
+//       `Error serve non raggiungibile: ${err.message} - ${err.code}`
+//     );
+//   })
+//   .end();
+
 const options = {
-  hostname: "info.cern.cch",
+  hostname: "it.wikipedia.org",
   method: "GET",
-  path: "/hypertext/WWW/TheProject.html",
+  path: "/wiki/Node.js",
 };
 
-const req = http
+const req = https
   .request(options, (res) => {
     if (res.statusCode < 300 && res.statusCode >= 200) {
       console.log(`Request successful: ${res.statusCode}`);
