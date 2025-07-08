dc6a3fc8556edf23dac84e352c69216c5201077a
MapoDesign <elisaresmini@gmail.com>
2025-07-08 13:00:47 +0200

    update getWebsite - download image

--- components/getWebsite.js
+++ components/getWebsite.js
@@ -25,26 +25,47 @@ const path = require("path");
 //   })
 //   .end();
 
-const options = {
-  hostname: "it.wikipedia.org",
-  method: "GET",
-  path: "/wiki/Node.js",
-};
+// const options = {
+//   hostname: "it.wikipedia.org",
+//   method: "GET",
+//   path: "/wiki/Node.js",
+// };
 
-const req = https
-  .request(options, (res) => {
-    if (res.statusCode < 300 && res.statusCode >= 200) {
-      console.log(`Request successful: ${res.statusCode}`);
-      res.on("data", (chunk) => {
-        process.stdout.write(chunk);
-      });
+// const req = https
+//   .request(options, (res) => {
+//     if (res.statusCode < 300 && res.statusCode >= 200) {
+//       console.log(`Request successful: ${res.statusCode}`);
+//       res.on("data", (chunk) => {
+//         process.stdout.write(chunk);
+//       });
+//     } else {
+//       console.log(`Request failed: ${res.statusCode}`);
+//     }
+//   })
+//   .on("error", (err) => {
+//     console.error(
+//       `Error serve non raggiungibile: ${err.message} - ${err.code}`
+//     );
+//   })
+//   .end();
+
+// Download an image from a URL and save it to a file
+const fs = require("fs");
+const file = fs.createWriteStream("nodejs-logo.png");
+
+const url =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/250px-Node.js_logo.svg.png";
+
+https
+  .get(url, (response) => {
+    if (response.statusCode === 200) {
+      response.pipe(file);
     } else {
-      console.log(`Request failed: ${res.statusCode}`);
+      console.error(
+        `Failed to download image. Status code: ${response.statusCode}`
+      );
     }
   })
   .on("error", (err) => {
-    console.error(
-      `Error serve non raggiungibile: ${err.message} - ${err.code}`
-    );
-  })
-  .end();
+    console.error(`Error downloading image: ${err.message}`);
+  });
